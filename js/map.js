4e0ec7f0e752b5c0cce02a136b9b5e4bea8c1852
Tiboeva Angela <tas14@yandex.ru>
2017-12-05 15:03:45 +0300

    Доделала выбор мест гостей по умолчанию при открытии

--- js/map.js
+++ js/map.js
@@ -179,8 +179,21 @@ pinMain.addEventListener('mouseup', onPinMouseup);
 // задание о валидации
 var timeIn = document.querySelector('#timein');
 var timeOut = document.querySelector('#timeout');
+var typeElement = document.querySelector('#type'); // валидация
+var address = document.querySelector('#address'); // валидация
+var titleForm = document.querySelector('#title'); // валидация
+var price = document.querySelector('#price');
+var roomNumber = document.querySelector('#room_number');
+var capacity = document.querySelector('#capacity');
+var buttonSubmit = document.querySelector('.form__submit');
+var input = document.querySelectorAll('input');
 
-
+var minPriceTypes = {
+  'bungalo': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
+};
 
 var onTimeinChange = function (evn) {
   var timeInChoice = evn.currentTarget;
@@ -192,20 +205,61 @@ var onTimeoutChange = function (evn) {
   timeIn.value = timeOutChoice.value;
 };
 
-var minPriceTypes = {
-  'bungalo': 0,
-  'flat': 1000,
-  'house': 5000,
-  'palace': 10000
+var setRooms = function () {
+  var roomChoice = roomNumber.value;
+  if (roomChoice === '1') {
+    capacity.value = '1';
+  } else if (roomChoice === '2') {
+    capacity.value = '2';
+  } else if (roomChoice === '3') {
+    capacity.value = '3';
+  } else if (roomChoice === '100') {
+    capacity.value = '0';
+  }
 };
 
-var typeElement = document.querySelector('#type');
-var price = document.querySelector('#price');
-var onTypeChange = function (evn) {
-  console.log(evn.currentTarget);
-  price.min = minPriceTypes[evn.currentTarget];
-  };
-onTypeChange();
-//typeElement.addEventListener('change', onTypeChange);
+var onRoomChange = function (evn) {
+  var value = evn.currentTarget.value;
+  setRooms(value);
+};
+
+var onButtonError = function () {
+  if (address.validity.valueMissing) {
+    address.setCustomValidity('Обязательное поле');
+    address.style.border = '2px solid red';
+  } else {
+    address.setCustomValidity('');
+    address.style.border = '1px solid #d9d9d3';
+  }
+  if (titleForm.validity.valueMissing) {
+    titleForm.setCustomValidity('Обязательное поле');
+    titleForm.style.border = '2px solid red';
+  } else {
+    titleForm.setCustomValidity('');
+    titleForm.style.border = '1px solid #d9d9d3';
+  }
+  if (price.validity.valueMissing) {
+    price.setCustomValidity('Обязательное поле');
+    price.style.border = '2px solid red';
+  } else {
+    price.setCustomValidity('');
+    price.style.border = '1px solid #d9d9d3';
+  }
+};
+
+var setMinPrice = function (value) {
+  price.min = minPriceTypes[value];
+};
+
+var onTypeChange = function (evt) {
+  var value = evt.currentTarget.value;
+  setMinPrice(value);
+};
+
+setRooms(roomNumber.value);
+setMinPrice(typeElement.value);
+roomNumber.addEventListener('change', onRoomChange);
+buttonSubmit.addEventListener('click', onButtonError);
+typeElement.addEventListener('change', onTypeChange);
 timeIn.addEventListener('change', onTimeinChange);
 timeOut.addEventListener('change', onTimeoutChange);
